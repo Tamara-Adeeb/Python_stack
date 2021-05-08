2487ebe5c34bd400ce6f06bb84684843a7bdab69
Tamara Adeeb <tamara.adeeb1@gmail.com>
2021-05-08 10:17:14 +0300

    oop assignments uploaded

--- _python (the underscore IS IMPORTANT)/OOP/Users_with_Bank_Accounts.py
+++ _python (the underscore IS IMPORTANT)/OOP/Users_with_Bank_Accounts.py
@@ -1,32 +1,66 @@
 class BankAccount:
     def __init__(self, int_rate, balance):
         self.balance = 0
-        self.interest_rate = int_rate
+        self.interest_rate = 0.01
     def deposit(self, amount):
         self.balance += amount
+        return self
     def withdraw(self, amount):
         if self.balance < self.balance - amount:
             print("Insufficient funds: Charging a $5 fee")
             self.balance -= 5
         else:
             self.balance -= amount
+        return self
     def display_account_info(self):
         print("Balance:",self.balance)
+        return self
     def yield_interest(self):
         if self.balance > 0:
             self.balance += self.balance * self.interest_rate
+        return self
+
+# class Users:
+#     def __init__(self,name,email):
+#         self.name = name 
+#         self.email = email
+#         self.account = BankAccount(int_rate, balance)
+#     def make_deposit(self, amount):
+#         self.account.deposit(amount)
+#     def make_withdrawal(self, amount):
+#         self.account.withdraw(amount)
+#     def display_user_balance(self):
+#         print("User:", self.name, ", Balance:", self.account.balance)
+#     def transfer_money(self, other_user, amount):
+#         self.account.withdraw(amount)
+#         other_user.make_deposit(amount)
 
 class Users:
-    def __init__(self,name,email):
+    def __init__(self,name,email,int_rate = 0.01,balance=0):
         self.name = name 
         self.email = email
-        self.account = BankAccount(int_rate, balance)
-    def make_deposit(self, amount):
-        self.account.deposit(amount)
-    def make_withdrawal(self, amount):
-        self.account.withdraw(amount)
-    def display_user_balance(self):
-        print("User:", self.name, ", Balance:", self.account.balance)
-    def transfer_money(self, other_user, amount):
-        self.account.withdraw(amount)
+        self.account = [BankAccount(int_rate, balance)]
+    def make_account (self,int_rate=0.01, balance=0):
+        self.account.append(BankAccount(int_rate, balance))
+    def make_deposit(self, amount,index=0):
+        self.account[index].deposit(amount)
+        return self
+    def make_withdrawal(self, amount,index=0):
+        self.account[index].withdraw(amount)
+        return self
+    def display_user_balance(self.index):
+        print("User:", self.name, ", Balance:", self.account[index].balance)
+        return self
+    def transfer_money(self, other_user, amount,index):
+        self.account[index].withdraw(amount)
         other_user.make_deposit(amount)
+        return self
+    
+Tamara = Users("Tamara Adeeb","email" )
+Adeeb = Users("Adeeb Adeeb","email")
+Ali = Users("ALi Adeeb", "email")
+
+Tamara.make_deposit(1000).display_user_balance()
+Tamara.make_deposit(400).display_user_balance()
+
+
