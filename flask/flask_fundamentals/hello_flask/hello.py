4c0093f1e60b51e862833aef860f8b0081b08510
Tamara Adeeb <tamara.adeeb1@gmail.com>
2021-05-14 12:25:40 +0300

    update my first flask file 'hello'

--- flask/flask_fundamentals/hello_flask/hello.py
+++ flask/flask_fundamentals/hello_flask/hello.py
@@ -9,9 +9,19 @@ def home():
 def user(name):
     return f'<h1 style="text-align:center;" > Hello {name} </h1><p style="text-align:center;">Welcome ! <br> We are happy to have you</p>'
 
+@app.route("/<name1>/<name2>/<name3>")
+def users(name1,name2,name3):
+    names = [name1,name2,name3]
+    new_var = ""
+    for i in names:
+        new_var += f" <h1 style='text-align:center; '> Hello {i} </h1>\n"
+
+    return f'{new_var} <p style="text-align:center;">Welcome ! <br> We are happy to have you</p>'
+
+
 @app.route("/admin")
-def admin():
-    return redirect(url_for("home"))
+def admin(): # To redirect the user if they enter unauthorized route
+    return redirect(url_for("home")) #invoke home function
 
 if __name__ == "__main__":
     app.run(port="5001", debug=True)
