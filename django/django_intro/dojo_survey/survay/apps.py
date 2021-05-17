c3816e8551ca2dd8fa44aaedc4b0274dc9cb360c
Tamara Adeeb <tamara.adeeb1@gmail.com>
2021-05-17 16:20:14 +0300

    working in first django project

---
+++ django/django_intro/dojo_survey/survay/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class SurvayConfig(AppConfig):
+    name = 'survay'
--- django/django_intro/form_test/form_app/views.py
+++ django/django_intro/form_test/form_app/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 def index(request):
     return render(request,"index.html")
 
+
+
 def info(request):
     print(request.POST) #output:<QueryDict: 
     #{'csrfmiddlewaretoken': ['ieXl6P0Z2tTnX3s4ZUZ2iPCrPLgVMYyCev4oIDe6Py617SkqDuzxM8VM0wgZPmhc'], 
