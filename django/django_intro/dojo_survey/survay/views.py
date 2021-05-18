ce5e442d6d9f570ff03b4652bdd6680900da8c1e
Tamara Adeeb <tamara.adeeb1@gmail.com>
2021-05-18 11:11:10 +0300

    working in Dojo_survay folder using django

--- django/django_intro/dojo_survey/survay/views.py
+++ django/django_intro/dojo_survey/survay/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+def login_form(request):
+    # location = ["Palestine", "China", "Japan"]
+    # gender = ["Female", "Male"]
+    # language = ["Python","Marne","Java","JavaScript"]
+    context = {
+        "location" : ["Palestine", "China", "Japan"],
+        "gender" : ["Female", "Male"],
+        "language" : ["Python","Marne","Java","JavaScript"]
+    }
+    return render(request,"index.html",context)
+
+def show(request):
+    if request.method == "POST":
+        list = []
+        for i in request.POST:
+            list.append(request.POST[i])
+        context = {
+            "list" : list
+        }
+        return render(request,"user_info.html",context)
