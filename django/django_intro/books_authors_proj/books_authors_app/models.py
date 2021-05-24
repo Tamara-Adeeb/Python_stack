26ba82c9bc272277ea2d03e0fe6a34d49cfea14c
Tamara Adeeb <tamara.adeeb1@gmail.com>
2021-05-24 10:31:31 +0300

    working with django orm

--- django/django_intro/books_authors_proj/books_authors_app/models.py
+++ django/django_intro/books_authors_proj/books_authors_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.db.models.fields import TimeField
 
-# Create your models here.
+class Books(models.Model):
+    title = models.CharField(max_length=255)
+    desc = models.TextField()
+    created_at = models.TimeField(auto_now_add=True)
+    updated_at = models.TimeField(auto_now=True)
+
+class Authors(models.Model):
+    first_name = models.CharField(max_length=45)
+    last_name = models.CharField(max_length=45)
+    Books = models.ManyToManyField(Books,related_name="author")
+    notes = models.TextField(null=True)
+    created_at = models.TimeField(auto_now_add=True)
+    updated_at = models.TimeField(auto_now=True)
